a10f07ca26edd1ecd36c5f37ec4c701c9387e99c
sumanthpoola98 <chinna.poola98@gmail.com>
2024-09-11 00:27:43 +0530

    feat: add CustomerExistException.

---
+++ src/com/trainingmug/foodieapp/exceptions/CustomerExistException.java
@@ -0,0 +1,7 @@
+package com.trainingmug.foodieapp.exceptions;
+
+public class CustomerExistException extends Exception{
+    public CustomerExistException(String message){
+        super(message);
+    }
+}
